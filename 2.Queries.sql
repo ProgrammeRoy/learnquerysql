bd042b1a015e25f48eafaeba944b56747c502ed4
Samu Marw <sam_chacla@hotmail.com>
2018-05-02 08:07:15 -0500

    ejercicios agregados

---
+++ 2.Queries.sql
@@ -0,0 +1,20 @@
+1. Obtener los datos completos de los empleados.
+2. Obtener los datos completos de los departamentos
+3. Obtener los datos de los empleados con cargo 'Secretaria'.
+4. Obtener el nombre y salario de los empleados.
+5. Obtener los datos de los empleados vendedores, ordenado por nombre.
+6. Listar el nombre de los departamentos
+7. Listar el nombre de los departamentos, ordenado por nombre
+8. Listar el nombre de los departamentos, ordenado por ciudad
+9. Listar el nombre de los departamentos, ordenado por ciudad, en orden inverso
+10. Obtener el nombre y cargo de todos los empleados, ordenado por salario
+11. Obtener el nombre y cargo de todos los empleados, ordenado por cargo y por salario
+12. Obtener el nombre y cargo de todos los empleados, en orden inverso por cargo
+13. Listar los salarios y comisiones de los empleados del departamento 2000
+14. Listar los salarios y comisiones de los empleados del departamento 2000, ordenado por comisión
+15. Listar todas las comisiones
+16. Listar las comisiones que sean diferentes, ordenada por valor
+17. Listar los diferentes salarios
+18. Obtener el valor total a pagar que resulta de sumar a los empleados del departamento 3000 una
+bonificación de $500.000, en orden alfabético del empleado
+19. Obtener la lista de los empleados que ganan una comisión superior a su sueldo.
